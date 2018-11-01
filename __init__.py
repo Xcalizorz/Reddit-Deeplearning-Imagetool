8352b79888dd32a96d729e78f86ad33b247fa577
rezaizad <zadjad.rezai@draeger.com>
2018-11-01 09:02:27 +0100

    Credits change

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ that subreddit.
 """
 __maintainer__ = "Zadjad Rezai"
 __email__ = "zadjad.rezai@outlook.com"
-__credits__ = ["Zadjad Rezai", "Marcel Schultz"]
+__credits__ = ["Zadjad Rezai", ]
 
 __version__ = "ALPHA"
 __status__ = "IN DEVELOPMENT"
--- tests/__init__.py
+++ tests/__init__.py
@@ -8,7 +8,7 @@ Tests written for the Reddit-Deeplearning-Imagetool
 
 __maintainer__ = "Zadjad Rezai"
 __email__ = "zadjad.rezai@outlook.com"
-__credits__ = ["Zadjad Rezai", "Marcel Schultz"]
+__credits__ = ["Zadjad Rezai", ]
 
 __version__ = "ALPHA"
 __status__ = "IN DEVELOPMENT"
